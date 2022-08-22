595200fa102b3e9001f318316a257ee178d2b396
Ignace Pierre <ipierre354@gmail.com>
2022-08-22 13:03:42 -0400

    Feat: Created python file (main.py) new project

---
+++ main.py
@@ -0,0 +1,28 @@
+# Main Stories
+ 
+# (5 points): As a developer, I want to make at least five commits on GitHub with descriptive  messages.
+
+# (10 points): As a developer, I want every function to be a pure function that does not modify or  access global variables. 
+# (Receives data through parameters and returns data through the  “return” keyword).
+
+# (5 points): As a pair player, I want a fifty-two (52) deck of cards to be created and stored in a List  (number/type only, no suits). 
+        
+        # Four of each: Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10,  Jack, Queen, King
+        
+        #  HINT: Create a list called “deck”. This list should contain simple string values. For example, the list would contain four 
+        # (4) “7” string values to represent the four sevens (7) present in the deck.
+            
+            # deck = [“Ace”, “Ace”, “Ace”, “Ace”, “2”, “2”, “2”, “2”, “3” ..... ]
+
+                # How could you add them using a loop rather than having to type out every value?
+ 
+# (2.5 points): As a pair player, I want to ensure that only four (4) of each number/type exist within the shuffled deck.
+ 
+# (5 points): As a pair player, I want to be able to shuffle the deck of cards so that they are in random order.
+ 
+# (5 points): As a pair player, I want five (5) cards dealt to four (4) players. 
+ 
+# (5 points): As a pair player, I want to determine the number of pairs within each player’s hand and display the player number, 
+# their hand, and the number of pairs to the terminal. 
+
+# (5 points): As a pair player, I want to determine the game’s winner or tied players and display them to the terminal.  
