4edc6252c1fb3ad9878d14bda8d6931179264138
Ignace Pierre <ipierre354@gmail.com>
2022-08-23 20:23:21 -0400

    feat: added function to determine number of pairs (if any) in each player hand

--- Player_pairs.py
+++ Player_pairs.py
@@ -1,33 +1,36 @@
 import random
 
-# Main Stories
+"""
+Main Stories
  
-# (5 points): As a developer, I want to make at least five commits on GitHub with descriptive  messages.
+(5 points): As a developer, I want to make at least five commits on GitHub with descriptive  messages.
 
-# (10 points): As a developer, I want every function to be a pure function that does not modify or  access global variables. 
-# (Receives data through parameters and returns data through the  “return” keyword).
+(10 points): As a developer, I want every function to be a pure function that does not modify or  access global variables. 
+(Receives data through parameters and returns data through the  “return” keyword).
 
-# (5 points): As a pair player, I want a fifty-two (52) deck of cards to be created and stored in a List  (number/type only, no suits). 
+(5 points): As a pair player, I want a fifty-two (52) deck of cards to be created and stored in a List  (number/type only, no suits). 
         
-        # Four of each: Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10,  Jack, Queen, King
+        Four of each: Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10,  Jack, Queen, King
         
-        #  HINT: Create a list called “deck”. This list should contain simple string values. For example, the list would contain four 
-        # (4) “7” string values to represent the four sevens (7) present in the deck.
+         HINT: Create a list called “deck”. This list should contain simple string values. For example, the list would contain four 
+        (4) “7” string values to represent the four sevens (7) present in the deck.
             
-            # deck = [“Ace”, “Ace”, “Ace”, “Ace”, “2”, “2”, “2”, “2”, “3” ..... ]
+            deck = [“Ace”, “Ace”, “Ace”, “Ace”, “2”, “2”, “2”, “2”, “3” ..... ]
 
-                # How could you add them using a loop rather than having to type out every value?
+                How could you add them using a loop rather than having to type out every value?
  
-# (2.5 points): As a pair player, I want to ensure that only four (4) of each number/type exist within the shuffled deck.
+(2.5 points): As a pair player, I want to ensure that only four (4) of each number/type exist within the shuffled deck.
  
-# (5 points): As a pair player, I want to be able to shuffle the deck of cards so that they are in random order.
+(5 points): As a pair player, I want to be able to shuffle the deck of cards so that they are in random order.
  
-# (5 points): As a pair player, I want five (5) cards dealt to four (4) players. 
+(5 points): As a pair player, I want five (5) cards dealt to four (4) players. 
  
-# (5 points): As a pair player, I want to determine the number of pairs within each player’s hand and display the player number, 
-# their hand, and the number of pairs to the terminal. 
+(5 points): As a pair player, I want to determine the number of pairs within each player’s hand and display the player number, 
+their hand, and the number of pairs to the terminal. 
 
-# (5 points): As a pair player, I want to determine the game’s winner or tied players and display them to the terminal.
+(5 points): As a pair player, I want to determine the game’s winner or tied players and display them to the terminal.
+
+"""
 
 
 def greet():
@@ -43,12 +46,18 @@ def run_player_pairs():
     'Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'
     'Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 
+# Shuffle the deck
+
     random.shuffle(deck)
+
+# Functions elements to ensure variable 'deck' passes through follow-up functions
 
     player_1(deck)
     player_2(deck)
     player_3(deck)
     player_4(deck)
+
+# Function for Player 1
 
 def player_1(deck):
     player_hand = 'Hand:' + ''
@@ -59,6 +68,8 @@ def player_1(deck):
 
     print(player_hand)
 
+    amount_of_pairs(player_hand)
+
 def player_2(deck):
     random.shuffle(deck)
     player_hand = 'Hand:' + ''
@@ -68,6 +79,8 @@ def player_2(deck):
         player_hand += ' ' + str(deck[draw]) + ','
 
     print(player_hand)
+
+    amount_of_pairs(player_hand)
 
 
 def player_3(deck):
@@ -80,6 +93,8 @@ def player_3(deck):
 
     print(player_hand)
 
+    amount_of_pairs(player_hand)
+
 
 def player_4(deck):
     random.shuffle(deck)
@@ -91,14 +106,17 @@ def player_4(deck):
 
     print(player_hand)
 
-run_player_pairs()
-
+    amount_of_pairs(player_hand)
 
 # Checking for pairs 
 
-# def check_for_pairs():
-#     values = sorted(card[0] for card in player_hand)
-#         if values.count(values[2]) == 2:
-#         two_of_a_kind += 1
+def amount_of_pairs(player_hand):
+    for card in player_hand:
+        if player_hand.count(card) == 2:
+            print('Number of Pairs: ' + str(player_hand.count(card)))
+        elif player_hand.count(card) == 0:
+            print('Number of Pairs: 0')
+
+run_player_pairs()
 
 # def who_won():
